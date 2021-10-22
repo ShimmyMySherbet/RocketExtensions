24a91b1f704a35b333f63f299f46f23aef3aeb2f
ShimmyMySherbet <ShimmyMySherbet@gmail.com>
2021-10-22 15:03:28 +1000

    Added more chat tools and cooldown canceling

--- RocketExtensions/Models/CommandContext.cs
+++ RocketExtensions/Models/CommandContext.cs
@@ -1,5 +1,6 @@
 ﻿using Rocket.API;
 using Rocket.Unturned.Chat;
+using Rocket.Unturned.Player;
 using RocketExtensions.Utilities.ShimmyMySherbet.Extensions;
 using System.Threading.Tasks;
 using UnityEngine;
@@ -15,23 +16,63 @@ namespace RocketExtensions.Models
         public string[] CommandRawArguments { get; private set; }
         public ArgumentList Arguments { get; private set; }
 
-        public CommandContext(IRocketPlayer player, string[] args)
+        /// <summary>
+        /// The caller as an UnturnedPlayer. Null when called from console.
+        /// </summary>
+        public UnturnedPlayer UnturnedPlayer => Player as UnturnedPlayer ?? null;
+
+        public IRocketCommand Command { get; }
+
+        public CommandContext(IRocketPlayer player, IRocketCommand callingCommand, string[] args)
         {
+            Command = callingCommand;
             Player = player;
             CommandRawArguments = args;
             Arguments = new ArgumentList(args);
         }
 
+        /// <summary>
+        /// Sends a message to the caller
+        /// </summary>
         public async Task ReplyAsync(string message, Color? messageColor = null)
         {
             if (messageColor == null)
             {
                 messageColor = Color.green;
             }
-            await ThreadTool.RunOnGameThreadAsync((IRocketPlayer player, string msg) =>
+            await ThreadTool.RunOnGameThreadAsync(UnturnedChat.Say, Player, message, messageColor.Value);
+        }
+
+        /// <summary>
+        /// Sends a message to the specified player
+        /// </summary>
+        public async Task SayAsync(IRocketPlayer player, string message, Color? messageColor = null)
+        {
+            if (messageColor == null)
             {
-                UnturnedChat.Say(player, msg, messageColor.Value, true);
-            }, Player, message);
+                messageColor = Color.green;
+            }
+            await ThreadTool.RunOnGameThreadAsync(UnturnedChat.Say, player, message, messageColor.Value);
+        }
+
+        /// <summary>
+        /// Sends a message to all online players.
+        /// </summary>
+        public async Task Announceasync(string message, Color? messageColor = null)
+        {
+            if (messageColor == null)
+            {
+                messageColor = Color.green;
+            }
+            await ThreadTool.RunOnGameThreadAsync(UnturnedChat.Say, message, messageColor.Value);
+        }
+
+        /// <summary>
+        /// Cancels the command cooldown for the current command.
+        /// </summary>
+        public async Task<bool> SendCancelCooldown()
+        {
+            return await ThreadTool.RunOnGameThreadAsync(CooldownManager.CancelCooldown, Player, Command);
         }
     }
 }
---
+++ RocketExtensions/Models/CooldownManager.cs
@@ -0,0 +1,47 @@
+﻿using Rocket.API;
+using Rocket.Core;
+
+namespace RocketExtensions.Models
+{
+    public static class CooldownManager
+    {
+        private static dynamic GetCooldownItem(IRocketPlayer player, IRocketCommand command)
+        {
+            dynamic r = R.Commands;
+
+            foreach (dynamic cooldown in r.cooldown)
+            {
+                if (cooldown.Player == player && cooldown.Command == command)
+                {
+                    return cooldown;
+                }
+            }
+
+            return null;
+        }
+
+        public static bool CancelCooldown(IRocketPlayer player, IRocketCommand command)
+        {
+            var cooldown = GetCooldownItem(player, command);
+            if (cooldown == null)
+            {
+                return false;
+            }
+            RemoveCooldown(cooldown);
+            return true;
+        }
+
+        private static void RemoveCooldown(dynamic cooldown)
+        {
+            if (cooldown != null)
+            {
+                dynamic r = R.Commands;
+
+                if (r.cooldown.Contains(cooldown))
+                {
+                    r.cooldown.Remove(cooldown);
+                }
+            }
+        }
+    }
+}
